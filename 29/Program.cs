7e7962c184d6a3457e7f7d3c0f6b6ca8ce054ff4
Andrei Aliaksandrau <aleksandrov.a.95@yandex.ru>
2023-01-14 18:46:26 +0300

    29 added but not finished

---
+++ 29/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Clear();
+createArray();
+
+void createArray() 
+{
+    int[] array = new int[8];
+    for (int i = 0; i < 8; i++) 
+    {
+        array[i] = int.Parse(Console.ReadLine() ?? "");
+    }
+}

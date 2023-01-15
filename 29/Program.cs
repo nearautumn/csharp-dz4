e5f270b925e0f80c60ad5d4d13ee6db42f4cced3
Andrei Aliaksandrau <aleksandrov.a.95@yandex.ru>
2023-01-15 21:12:14 +0300

    29 finished

--- 29/Program.cs
+++ 29/Program.cs
@@ -1,13 +1,38 @@
 ﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
 Console.Clear();
-createArray();
-
-void createArray() 
+int[] array = new int[8];
+int n = 0;
+for (int i = 0; i < 8; i++) 
 {
-    int[] array = new int[8];
-    for (int i = 0; i < 8; i++) 
+    array[i] = GetNumberFromUser($"Ввдеите целое число (элемент массива № {n}): ", "Ошибка ввода!");
+    n++;
+}
+Console.Clear();
+printArray(array);
+
+int GetNumberFromUser(string message, string errorMessage) 
+{
+    while(true) 
     {
-        array[i] = int.Parse(Console.ReadLine() ?? "");
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
     }
+}
+
+void printArray(int[] numbers) 
+{
+    for (int index = 0; index < 7; index++) 
+    {
+        Console.Write($"{numbers[index]}, ");
+    }
+    Console.Write($"{numbers[7]} -> [");
+    for (int index = 0; index < 7; index++) 
+    {
+        Console.Write($"{numbers[index]}, ");
+    }
+    Console.Write($"{numbers[7]}]");
 }

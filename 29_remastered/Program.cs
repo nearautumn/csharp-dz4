98ebb6f4652475a0fd65628c7ea7ad2c41bf8650
Andrei Aliaksandrau <aleksandrov.a.95@yandex.ru>
2023-01-17 19:34:59 +0300

    29 remastered added

---
+++ 29_remastered/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = GetRandomArray(8, 0, 100);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+///////////////////////////////////////
+
+int[] GetRandomArray(int size, int minValue, int maxValue) 
+{
+    int[] result = new int[size];
+    var rnd = new Random();
+    for(int i = 0; i < size; i++)
+    {
+        result[i] = rnd.Next(minValue, maxValue);
+    }
+    return result;
+}
